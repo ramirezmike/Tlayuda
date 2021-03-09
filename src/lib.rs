a1a9c64397a78158d37a157ae639e595e377608b
mramirez <ramirezmike2@gmail.com>
2021-03-08 22:07:23 -0500

    Added dynamic set functions that work only with strings

--- src/lib.rs
+++ src/lib.rs
@@ -2,39 +2,63 @@ use proc_macro::TokenStream;
 use syn::{parse_macro_input, ItemStruct, Fields, FieldsNamed};
 use quote::quote;
 
-#[proc_macro]
-pub fn make_answer(_item: TokenStream) -> TokenStream {
-    "fn answer() -> u32 { 42 }".parse().unwrap()
-}
+use std::sync::atomic::{AtomicBool, Ordering};
 
+static BUILDER_DECLARED: AtomicBool = AtomicBool::new(false);
 
 #[proc_macro_derive(tob)]
-pub fn tob(item: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as ItemStruct);
+pub fn tob(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as ItemStruct);
     let fields = input.fields
                       .iter()
                       .filter(|x| x.ident.is_some())
                       .map(|x| x.ident.as_ref());
 
     let name = &input.ident;
-    let builder_name = quote::format_ident!("tob_{}_builder", name);
+
+    let object_builder_declaration = create_object_builder_declaration();
+
+    let inner_builder_name = quote::format_ident!("tob_inner_{}_builder", name);
+
+    let fields_1 = fields.clone();
+    let fields_2 = fields.clone();
+    let fields_3 = fields.clone();
+    let fields_4 = fields.clone();
+
+    let funcs = fields.clone().map(|x| {
+                    let func_name = quote::format_ident!("set_{}", x.unwrap());
+                    quote! {
+                        pub fn #func_name<F: 'static>(mut self, f: F) -> Self where
+                            F: Fn(usize) -> String {
+                                self.#x = Some(Box::new(f));
+                                self
+                        }
+                    }
+                });
 
     let output = quote! {
-        struct #builder_name {
-            index: usize
+        #object_builder_declaration
+
+        struct #inner_builder_name {
+            index: usize,
+            #(#fields_2: Option<Box<dyn FnMut(usize) -> String>>),*
         }
-        impl #builder_name {
-            pub fn new() -> Self {
-                #builder_name {
-                    index: 0
+
+        impl #inner_builder_name {
+            pub fn new() -> #inner_builder_name {
+                #inner_builder_name {
+                    index: 0,
+                    #(#fields_3: None),* 
                 }
             }
+
+            #(#funcs)*
 
             fn take_index(&mut self) -> usize {
                 self.index = self.index + 1;
                 self.index - 1
             }
-
+            
             pub fn build(&mut self) -> #name {
                 let i = self.take_index();
                 #name {
@@ -47,12 +71,25 @@ pub fn tob(item: TokenStream) -> TokenStream {
             }
         }
 
-        impl #name {
-            pub fn tob() -> #builder_name {
-                #builder_name::new()
+        impl ObjectBuilder<#name> {
+            pub fn new() -> #inner_builder_name {
+                #inner_builder_name::new()
             }
         }
     };
 
     TokenStream::from(output)
+}
+
+fn create_object_builder_declaration() -> proc_macro2::TokenStream {
+    if BUILDER_DECLARED.load(Ordering::Relaxed) {
+        return quote! {};
+    } 
+
+    BUILDER_DECLARED.store(true, Ordering::SeqCst);
+    quote! {
+        struct ObjectBuilder<T> { 
+            phantom: std::marker::PhantomData<T>
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,67 @@ struct Person {
     ssn: String,
 }
 
-fn main() {
-    let p = Person::tob().build();
-    println!("hello {:?} ", p.first_name);
-    println!("hello {:?} ", p.last_name);
-    println!("hello {:?} ", p.ssn);
+#[derive(tob)]
+struct Teacher {
+    first_name: String,
+    last_name: String,
+    ssn: String,
+}
 
-    Person::tob()
-           .build_vec()
-           .iter()
-           .for_each(|x| println!("Hello {} {} {}", x.first_name, x.last_name, x.ssn));
+struct InnerPob {
+    thing : Option<Box<dyn FnMut(usize) -> String>>
+}
+impl InnerPob {
+    pub fn new() -> InnerPob {
+        InnerPob {
+            thing: None
+        }
+    }
+
+    pub fn set_thing<F: 'static>(mut self, f: F) -> Self where
+        F: Fn(usize) -> String {
+            self.thing = Some(Box::new(f));
+            self
+    }
+
+    pub fn build(&mut self) -> Person {
+        let t = self.thing.as_mut().unwrap();
+        Person {
+            first_name: t(0),
+            last_name: t(1),
+            ssn: t(2)
+        }
+    }
+}
+
+struct OB<T> {
+    phantom: std::marker::PhantomData<T>
+}
+
+impl OB<Person> {
+    pub fn new() -> InnerPob {
+        InnerPob::new()
+    }
+}
+
+fn main() {
+    let mut  x = OB::<Person>::new();
+    x = x.set_thing(|i| format!("blaha{}", i).into());
+    let result = x.build();
+    println!("Hello Test {} {} {}", result.first_name, result.last_name, result.ssn);
+
+
+    let mut p = ObjectBuilder::<Person>::new();
+    let p = p.set_first_name(|i| format!("baa{}", i).into())
+             .build();
+    println!("Hello Person {} {} {}", p.first_name, p.last_name, p.ssn);
+
+    let mut p = ObjectBuilder::<Person>::new();
+    p.build_vec()
+     .iter()
+     .for_each(|x| println!("Hello Persons {} {} {}", x.first_name, x.last_name, x.ssn));
+
+    let mut p = ObjectBuilder::<Teacher>::new();
+    let p = p.build();
+    println!("Hello Teacher {} {} {}", p.first_name, p.last_name, p.ssn);
 }
